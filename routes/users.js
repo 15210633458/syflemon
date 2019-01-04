b6dcf19d0b93e59083be450d5890c31d2926beea
sunyufeng <2220438231@qq.com>
2019-01-04 19:09:47 +0800

    用户添加

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('../mongodb')
+    //console.log(mongodb)
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+//添加用户
+router.get('/api/addUser', function(req, res, next) {
+    var user = req.query.user || '';
+    mongodb.getmongodb("user", function(err, con, coll) {
+        if (err) {
+            return res.json({
+                code: 0,
+                mes: err
+            })
+        }
+        coll.insert({ user: user }, function(error, result) {
+            if (error) {
+                return res.json({
+                    code: 0,
+                    mes: error
+                })
+            }
+            //console.log(result.insertedIds[0])
+            res.json({
+                code: 1,
+                id: result.insertedIds[0]
+            })
+        })
+    })
+})
 module.exports = router;
