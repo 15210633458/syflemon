6e5dbe91ae2f7cb42b57c2e14eadd6274ee1b978
sunyufeng <2220438231@qq.com>
2019-01-08 13:49:51 +0800

    查询分类

--- routes/classfy.js
+++ routes/classfy.js
@@ -9,31 +9,10 @@ var classfy = require('./classfy/index.js');
 //加载icon图标
 router.get('/api/icon', classfy.icon);
 
-// //添加分类
-// //查询分类
-// router.get('/api/findType', function(req, res, next) {
-//     mongodb.getmongodb("listbill", function(err, con, coll) {
-//         if (err) {
-//             res.json({ code: 0, mes: err })
-//         } else {
-//             var uid = req.query.uid;
-//             console.log(uid)
-//             coll.find({ $or: [{ uid: '*' }, { uid: uid }] }).toArray(function(error, result) {
-//                 if (error) {
-//                     res.json({
-//                         code: 0,
-//                         mes: error
-//                     })
-//                 } else {
-//                     console.log(result)
-//                     res.json({
-//                         code: 1,
-//                         data: result,
-//                     })
-//                 }
-//             })
-//         }
-//     })
-// })
 
+//查询分类
+router.get('/api/findType', classfy.findType)
+
+//添加分类
+router.get('/api/addType', classfy.addType)
 module.exports = router;
--- routes/classfy/index.js
+++ routes/classfy/index.js
@@ -18,6 +18,29 @@ function icon(req, res, next) {
 }
 
 //查询分类
+function findType(req, res, next) {
+    var uid = req.query.uid;
+    mongodb.find("listbill", { $or: [{ uid: '*' }, { uid: uid }] }, function(error, result) {
+        if (error) {
+            res.json({
+                code: 0,
+                mes: error
+            })
+        } else {
+            //console.log(result)
+            res.json({
+                code: 1,
+                data: result,
+            })
+        }
+    })
+}
+
+function addType(req, res, next) {
+
+}
 module.exports = {
-    icon: icon
+    icon: icon,
+    findType: findType,
+    addType: addType
 }
